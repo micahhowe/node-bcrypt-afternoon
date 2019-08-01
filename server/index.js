c67adb8dccfcd3956ce1d4cf4c6c86b16147c5ef
micahhowe <micahhowe182@gmail.com>
2019-08-01 15:05:54 -0600

    got the database set up

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express') 
+const session = require('express-session')
+const massive = require('massive')
+const PORT = 4000
+const {CONNECTION_STRING, SESSION_SECRET} = process.env
+
+const app = express()
+
+app.use(express.json())
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+  }))
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    app.listen(PORT, () => console.log(`Now listening on the following port: ${PORT}`))
+}).catch(err => {
+    console.log(`Dang couldn't connect to the db.`, err)
+})
+
